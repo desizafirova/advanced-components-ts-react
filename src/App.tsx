706f1f35749f1546eb08d62ec7f1185ec799f737
DesiZafirova <desszafirowa@gmail.com>
2025-01-14 16:53:32 +0200

    Exposing component APIs with useImperativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import Form from './components/Form';
+import Form, { type FormHandle } from './components/Form';
 import Input from './components/Input';
 import Button from './components/Button';
+import { useRef } from 'react';
 
 function App() {
+  //  make it clear that useRef will eventually refer to this FormHandle object
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
     // the built-in "as" keyword allows us to convert the type TS knows to a different type, if we know better than TS what a type is. Age here is also a string, because values entered into input fields are always type of string when we extract them. Even if the input field had type number(that's just affecting how it will be presented, but it's not affecting how the value is extracted).
     const extractedData = data as { name: string; age: string };
     console.log(extractedData);
+    customForm.current?.clear(); // Clears the form after submission
   }
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
         <p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,33 @@
-import { ComponentPropsWithoutRef, FormEvent } from 'react';
+import {
+  ComponentPropsWithoutRef,
+  FormEvent,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from 'react';
+
+export type FormHandle = {
+  clear: () => void;
+};
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
   onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...otherProps }: FormProps) {
+const Form = forwardRef<FormHandle, FormProps>(function Form(
+  { onSave, children, ...otherProps },
+  ref
+) {
+  const form = useRef<HTMLFormElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        form.current?.reset();
+      },
+    };
+  });
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -13,8 +36,10 @@ export default function Form({ onSave, children, ...otherProps }: FormProps) {
     onSave(data);
   }
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   );
-}
+});
+
+export default Form;
