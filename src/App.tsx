22fc2d9a155a1f87fc5d5838387ec0e53e355211
DesiZafirova <desszafirowa@gmail.com>
2025-01-15 17:27:12 +0200

    creating a context and provider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
-import Form, { type FormHandle } from './components/UI/Form';
-import Input from './components/UI/Input';
-import Button from './components/UI/Button';
-import { useRef } from 'react';
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
+import TimersContextProvider from './store/timers-context.tsx';
 
 function App() {
-  //  make it clear that useRef will eventually refer to this FormHandle object
-  const customForm = useRef<FormHandle>(null);
-
-  function handleSave(data: unknown) {
-    // the built-in "as" keyword allows us to convert the type TS knows to a different type, if we know better than TS what a type is. Age here is also a string, because values entered into input fields are always type of string when we extract them. Even if the input field had type number(that's just affecting how it will be presented, but it's not affecting how the value is extracted).
-    const extractedData = data as { name: string; age: string };
-    console.log(extractedData);
-    customForm.current?.clear(); // Clears the form after submission
-  }
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}>
-        <Input type="text" label="Name" id="name" />
-        <Input type="number" label="Age" id="age" />
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
-    </main>
+    <TimersContextProvider>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </TimersContextProvider>
   );
 }
 
