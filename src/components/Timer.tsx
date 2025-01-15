2f7f186ceeeb719e0cadc0921222ffdeaf1c4407
DesiZafirova <desszafirowa@gmail.com>
2025-01-16 00:48:27 +0200

    stop and start timers functionallity implemented

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,51 @@
-import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useEffect, useRef, useState } from 'react';
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from '../store/timers-context.tsx';
+import { preview } from 'vite';
 
 export default function Header({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [isRunning]);
+
+  const formatedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <header>
       <h1>{name}</h1>
 
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formatedRemainingTime}</p>
     </header>
   );
 }
